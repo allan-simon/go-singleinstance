1d5c23585a1c14b861ee3c4f3a991a30af4b0d04
Cory Locklear <clocklear@redventures.com>
2015-10-26 16:15:04 -0400

    Altered formatting to match style in source repo

--- lock_posix.go
+++ lock_posix.go
@@ -3,8 +3,8 @@
 package singleinstance
 
 import (
-	"os"
-	"syscall"
+    "os"
+    "syscall"
 )
 
 // CreateLockFile try to create a file with given name
@@ -12,25 +12,25 @@ import (
 // if the file already exists AND is still locked, it will fail
 func CreateLockFile(filename string) (*os.File, error) {
 
-	var (
-		file *os.File
-		err  error
-	)
+    var (
+        file *os.File
+        err  error
+    )
 
-	if _, err = os.Stat(filename); os.IsNotExist(err) {
-		// file doesnt exist, create
-		file, err = os.Create(filename)
-	} else {
-		// file does exist, open
-		file, err = os.OpenFile(filename, os.O_WRONLY, 0666)
-	}
+    if _, err = os.Stat(filename); os.IsNotExist(err) {
+        // file doesnt exist, create
+        file, err = os.Create(filename)
+    } else {
+        // file does exist, open
+        file, err = os.OpenFile(filename, os.O_WRONLY, 0666)
+    }
 
-	if err != nil {
-		return nil, err
-	}
-	err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
-	if err != nil {
-		return nil, err
-	}
-	return file, nil
+    if err != nil {
+        return nil, err
+    }
+    err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+    if err != nil {
+        return nil, err
+    }
+    return file, nil
 }
