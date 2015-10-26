c0c5c6a3a215002a05f1bb8cc133162a610c926f
Cory Locklear <clocklear@redventures.com>
2015-10-26 15:47:10 -0400

    Use filename instead of hardcoded string

--- lock_posix.go
+++ lock_posix.go
@@ -3,21 +3,21 @@
 package singleinstance
 
 import (
-    "os"
-    "syscall"
+	"os"
+	"syscall"
 )
 
 // CreateLockFile try to create a file with given name
 // and acquire an exclusive lock on it
 // if the file already exists AND is still locked, it will fail
 func CreateLockFile(filename string) (*os.File, error) {
-    file, err := os.OpenFile("plop", os.O_WRONLY, 0666)
-    if err != nil {
-        return nil, err
-    }
-    err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
-    if err != nil {
-        return nil, err
-    }
-    return file, nil
+	file, err := os.OpenFile(filename, os.O_WRONLY, 0666)
+	if err != nil {
+		return nil, err
+	}
+	err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
 }
