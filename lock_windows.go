d9e3350525c0e89dd5787965fbca5570b9b3df34
emersion <contact@emersion.fr>
2016-07-29 12:54:37 +0200

    Fixes lock_windows.go

--- lock_windows.go
+++ lock_windows.go
@@ -11,12 +11,13 @@ import (
 // exclusive lock on it. If the file already exists AND is still locked, it will
 // fail.
 func CreateLockFile(filename string) (*os.File, error) {
-	if _, err := os.Stat(filename); !os.IsNotExist(err) {
+	if _, err := os.Stat(filename); err == nil {
 		// If the files exists, we first try to remove it
-		err = os.Remove(filename)
-		if err != nil {
+		if err = os.Remove(filename); err != nil {
 			return nil, err
 		}
+	} else if !os.IsNotExist(err) {
+		return nil, err
 	}
 
 	file, err := os.OpenFile(filename, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0600)
