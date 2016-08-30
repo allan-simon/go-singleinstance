b936eb17a6508e247343438d62245c7eaba8e860
emersion <contact@emersion.fr>
2016-08-30 17:00:49 +0200

    Adds unit tests, improves example

---
+++ lock_test.go
@@ -0,0 +1,52 @@
+package singleinstance
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func createTestLock(t *testing.T) *os.File {
+	tmpfile, err := ioutil.TempFile("", "singleinstance")
+	if err != nil {
+		t.Fatal("Cannot create temporary file:", err)
+	}
+	tmpfile.Close()
+
+	f, err := CreateLockFile(tmpfile.Name())
+	if err != nil {
+		t.Fatal("Expected no error while creating lock, got:", err)
+	}
+
+	return f
+}
+
+func TestCreateLockFile(t *testing.T) {
+	f := createTestLock(t)
+	defer os.Remove(f.Name())
+
+	if _, err := CreateLockFile(f.Name()); err == nil {
+		t.Fatal("Expected an error while trying to lock, got:", err)
+	}
+
+	f.Close() // Remove the lock
+
+	if _, err := CreateLockFile(f.Name()); err != nil {
+		t.Fatal("Expected no error while trying to lock, got:", err)
+	}
+
+	f.Close()
+}
+
+func TestGetLockFilePid(t *testing.T) {
+	f := createTestLock(t)
+	defer os.Remove(f.Name())
+
+	pid, err := GetLockFilePid(f.Name())
+	if err != nil {
+		t.Fatal("Expected no error while getting PID, got:", err)
+	}
+	if pid != os.Getpid() {
+		t.Errorf("Invalid PID: expected %v but got %v", os.Getpid(), pid)
+	}
+}
